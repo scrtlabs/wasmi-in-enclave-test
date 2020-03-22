1116c87ddaf3a6b5e96ef4a1e8e3ee752a155086
assafmo <assaf.morami@gmail.com>
2020-03-23 00:37:36 +0200

    HeapMaxSize 1mb->2mb

--- samplecode/hello-wasmi/app/src/main.rs
+++ samplecode/hello-wasmi/app/src/main.rs
@@ -22,9 +22,13 @@ use sgx_types::*;
 use sgx_urts::SgxEnclave;
 static ENCLAVE_FILE: &'static str = "enclave.signed.so";
 
-extern {
-    fn say_something(eid: sgx_enclave_id_t, retval: *mut sgx_status_t,
-                     some_string: *const u8, len: usize) -> sgx_status_t;
+extern "C" {
+    fn say_something(
+        eid: sgx_enclave_id_t,
+        retval: *mut sgx_status_t,
+        some_string: *const u8,
+        len: usize,
+    ) -> sgx_status_t;
 }
 
 fn init_enclave() -> SgxResult<SgxEnclave> {
@@ -33,12 +37,17 @@ fn init_enclave() -> SgxResult<SgxEnclave> {
     // call sgx_create_enclave to initialize an enclave instance
     // Debug Support: set 2nd parameter to 1
     let debug = 1;
-    let mut misc_attr = sgx_misc_attribute_t {secs_attr: sgx_attributes_t { flags:0, xfrm:0}, misc_select:0};
-    SgxEnclave::create(ENCLAVE_FILE,
-                       debug,
-                       &mut launch_token,
-                       &mut launch_token_updated,
-                       &mut misc_attr)
+    let mut misc_attr = sgx_misc_attribute_t {
+        secs_attr: sgx_attributes_t { flags: 0, xfrm: 0 },
+        misc_select: 0,
+    };
+    SgxEnclave::create(
+        ENCLAVE_FILE,
+        debug,
+        &mut launch_token,
+        &mut launch_token_updated,
+        &mut misc_attr,
+    )
 }
 
 fn main() {
@@ -46,15 +55,14 @@ fn main() {
         Ok(r) => {
             println!("[+] Init Enclave Successful {}!", r.geteid());
             r
-        },
+        }
         Err(x) => {
             println!("[-] Init Enclave Failed {}!", x.as_str());
             return;
-        },
+        }
     };
 
-    let wasm_binary: Vec<u8> =
-    wabt::wat2wasm(
+    let wasm_binary: Vec<u8> = wabt::wat2wasm(
         r#"
         (module
             (func (export "test") (result i32)
@@ -68,15 +76,17 @@ fn main() {
     // let input_string = String::from("This is a normal world string passed into Enclave!\n");
     let mut retval = sgx_status_t::SGX_SUCCESS;
 
+    println!("wasm binary size: {}", wasm_binary.len());
     let result = unsafe {
-        say_something(enclave.geteid(),
-                      &mut retval,
-                      (&*wasm_binary).as_ptr() as * const u8,
-                      wasm_binary.len())
+        say_something(
+            enclave.geteid(),
+            &mut retval,
+            (&*wasm_binary).as_ptr() as *const u8,
+            wasm_binary.len(),
+        )
     };
-    
     match result {
-        sgx_status_t::SGX_SUCCESS => {},
+        sgx_status_t::SGX_SUCCESS => {}
         _ => {
             println!("[-] ECALL Enclave Failed {}!", result.as_str());
             return;
