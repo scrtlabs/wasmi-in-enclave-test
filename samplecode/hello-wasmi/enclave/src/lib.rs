f5c26035e444835aaa87f680ce4aff1f7a9c98be
assafmo <assaf.morami@gmail.com>
2020-03-22 17:16:11 +0200

    compiles :man_shrugging:

--- samplecode/hello-wasmi/enclave/src/lib.rs
+++ samplecode/hello-wasmi/enclave/src/lib.rs
@@ -17,12 +17,11 @@
 
 #![crate_name = "helloworldsampleenclave"]
 #![crate_type = "staticlib"]
-
 #![cfg_attr(not(target_env = "sgx"), no_std)]
 #![cfg_attr(target_env = "sgx", feature(rustc_private))]
 
-extern crate sgx_types;
 extern crate sgx_trts;
+extern crate sgx_types;
 #[cfg(not(target_env = "sgx"))]
 #[macro_use]
 extern crate sgx_tstd as std;
@@ -34,60 +33,40 @@ extern crate wasmi;
 // use parity_wasm::elements::{External, FunctionType, Internal, Type, ValueType};
 use wasmi::{ImportsBuilder, ModuleInstance, NopExternals, RuntimeValue};
 
-use sgx_types::*;
-use sgx_types::metadata::*;
 use sgx_trts::enclave;
+use sgx_types::metadata::*;
+use sgx_types::*;
 //use sgx_trts::{is_x86_feature_detected, is_cpu_feature_supported};
-use std::string::String;
-use std::vec::Vec;
+use std::backtrace::{self, PrintFormat};
 use std::io::{self, Write};
 use std::slice;
-use std::backtrace::{self, PrintFormat};
+use std::string::String;
+use std::vec::Vec;
 
 #[no_mangle]
 pub extern "C" fn say_something(some_string: *const u8, some_len: usize) -> sgx_status_t {
-
     // Ocall to normal world for output
     // println!("{}", &hello_string);
 
-    let _ = backtrace::enable_backtrace("enclave.signed.so", PrintFormat::Full);
+    // let _ = backtrace::enable_backtrace("enclave.signed.so", PrintFormat::Full);
 
-    // // Parse WAT (WebAssembly Text format) into wasm bytecode.
-    // let wasm_binary: Vec<u8> =
-    //     wabt::wat2wasm(
-    //         r#"
-    //         (module
-    //             (func (export "test") (result i32)
-    //                 i32.const 1337
-    //             )
-    //         )
-    //         "#,
-    //     )
-    //     .expect("failed to parse wat");
-    
+    let slice = unsafe { slice::from_raw_parts(some_string, some_len) };
 
     // // Load wasm binary and prepare it for instantiation.
-    let module = wasmi::Module::from_buffer(&some_string)
-        .expect("failed to load wasm");
+    let module = wasmi::Module::from_buffer(&slice).expect("failed to load wasm");
 
     // Instantiate a module with empty imports and
     // assert that there is no `start` function.
-    let instance =
-        ModuleInstance::new(
-            &module,
-            &ImportsBuilder::default()
-        )
+    let instance = ModuleInstance::new(&module, &ImportsBuilder::default())
         .expect("failed to instantiate wasm module")
         .assert_no_start();
 
     // // Finally, invoke the exported function "test" with no parameters
     // // and empty external function executor.
     assert_eq!(
-        instance.invoke_export(
-            "test",
-            &[],
-            &mut NopExternals,
-        ).expect("failed to execute export"),
+        instance
+            .invoke_export("test", &[], &mut NopExternals,)
+            .expect("failed to execute export"),
         Some(RuntimeValue::I32(1337)),
     );
 
